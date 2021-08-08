f920ac110a13e98f1fdda4f1185404be5b91fbc5
Andre Felipe <neoandfufe42@gmail.com>
2021-08-08 20:08:10 -0300

    Alteraçoes pertinentes

--- Main.hs
+++ Main.hs
@@ -1,6 +1,7 @@
 import System.IO
 import Data.Char
-import Text.PrettyPrint.Annotated.HughesPJClass (Doc)
+import Data.List
+
 
 
 -- Aluno: André Felipe Fuck
@@ -12,22 +13,45 @@ import Text.PrettyPrint.Annotated.HughesPJClass (Doc)
     Definir uma função que, dado um documento, gera um índice das 
     palavras que ocorrem nesse documento.
 -}
+ -- Numerar as linha s do documento:
+
 
 
 -- Programa principal
 
  -- contruirIndice :: Doc -> [([Int], Palavra)]
 
-contruirIndice :: IO ()
-contruirIndice = do 
+construirIndice :: IO ()
+construirIndice = do 
      file_text <- readFile "texto.txt"
      let lower_file_text = map toLower file_text -- deixando todas as palavras em minusculo 
-     let higeniza_file_text = higieniza lower_file_text
-     let line_file_text = lines higeniza_file_text
-     print line_file_text
+     let line_file_text = lines lower_file_text
+     let hig_file_text = map higieniza line_file_text
+     let num_linhas = numLinhas hig_file_text
+     let num_palavras_bruto = map numeraPalavras num_linhas
+     let num_palavras = foldr (++) [] num_palavras_bruto
+     let ordenar_palavras = ordenar num_palavras
+     let agrupar_palavras = agrupar ordenar_palavras
+     let elinarRep_palavras = eliminarRep agrupar_palavras
+
+     print elinarRep_palavras
 
 -- Higienização 
 
-higieniza l =  [x | x <- l, isAlpha x || isSpace x]
-    
-    -- [y | y <- l, isSpace y] 
+higieniza ls =  [l | l <- ls, isAlpha l || isSpace l]
+
+maior_3 l = repetidos [p | p <- words(l), length p >= 3]
+
+numLinhas :: [b] -> [(Int, b)]
+numLinhas ls =  zip [n | n <- [1 .. length ls]] [l | l <- ls]
+
+numeraPalavras ls =  [(fst ls, p) | p <- maior_3 (snd ls)]
+
+repetidos [] = []
+repetidos (l: ls) = if l `elem` ls then repetidos ls else l: repetidos ls
+
+ordenar ls  = sortOn snd ls
+
+agrupar ls = [([fst n | n <- ls, (snd p) == (snd n)], snd p)| p <- ls]
+
+eliminarRep ls = repetidos ls
