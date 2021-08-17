8f4364d4ca343aff805576a768e78fc21de975d9
André Felipe Fuck <66512647+andreFelipeHFuck@users.noreply.github.com>
2021-08-17 17:58:33 -0300

    Add files via upload

--- Main.hs
+++ Main.hs
@@ -1,7 +1,5 @@
 import System.IO
 import Data.Char
-import Data.List
-
 
 
 -- Aluno: André Felipe Fuck
@@ -28,62 +26,76 @@ type Linha = String
 type Palavra = String 
 
 
--- construirIndice :: Doc -> [([Int], Palavra)]
-construirIndice :: IO ()
-construirIndice = do 
-     file_text <- readFile "texto.txt"
-     let lower_file_text = map toLower file_text -- deixando todas as palavras em minusculo 
-     let line_file_text = lines lower_file_text
-     let hig_file_text = map higieniza line_file_text
-     let num_linhas = numLinhas hig_file_text
-     let num_palavras_bruto = map numeraPalavras num_linhas
-     let num_palavras = foldr (++) [] num_palavras_bruto
-     let ordenar_palavras = ordenar num_palavras
-     let agrupar_palavras = agrupar ordenar_palavras
-     let elinarRep_palavras = eliminarRep agrupar_palavras
+main :: IO ()
+main = do putStr "Arquivo: "
+          nome <- getLine
+          txt <- readFile nome
+          imprimir (contruirIndice txt)
 
-     print elinarRep_palavras
+contruirIndice :: Doc -> [([Int], Palavra)]
+contruirIndice doc = do 
+                      let lower_txt = map toLower doc -- Passando todas as palavras para letra minúscula
+                      let line_file_text = lines lower_txt
+                      let hig_file_text = map higieniza line_file_text
+                      let num_linhas = numLinhas hig_file_text
+                      let num_palavras = numeraPalavras num_linhas
+                      let ordenar_palavras = ordenar num_palavras
+                      let agrupar_palavras = agrupar ordenar_palavras
+                      let elinarRep_palavras = eliminarRep agrupar_palavras
+                      elinarRep_palavras
+
+imprimir [] = putStrLn ""
+imprimir ((n, l):ls) = do 
+                       putStr l
+                       putStr "............................"
+                       putStrLn (show n)
+                       imprimir ls
 
 -- Higienização 
-
+higieniza :: [Char] -> [Char]
 higieniza ls =  [l | l <- ls, isAlpha l || isSpace l]
 
-maior_3 l = repetidos [p | p <- words(l), length p >= 3]
 
-numLinhas :: [b] -> [(Int, b)]
+maior_3 l =  repetidos [p | p <- words(l), length p >= 3]
+
+numLinhas :: [Linha] -> [(Int, Linha)]
 numLinhas ls =  zip [n | n <- [1 .. length ls]] [l | l <- ls]
 
-numeraPalavras ls =  [(fst ls, p) | p <- maior_3 (snd ls)]
+numeraPalavras :: [(Int, Linha)] -> [(Int, Palavra)]
+numeraPalavras ls = foldr (++) [] (numeraPalavras'' ls)
+
+numeraPalavras' ls =  [(fst ls, p) | p <- maior_3 (snd ls)]
+
+numeraPalavras'' ls = map numeraPalavras' ls
+ 
 
 repetidos [] = []
 repetidos (l: ls) = if l `elem` ls then repetidos ls else l: repetidos ls
 
---ordenar ls  = sortOn snd ls
 
---
-
-
-menor_valor :: Ord a1 => (a2, a1) -> [(a2, a1)] -> (a2, a1)
 menor_valor ini [] = ini
-menor_valor ini (l: ls) = if snd ini < snd l then menor_valor ini ls else menor_valor l ls
+menor_valor ini (l: ls) = if snd ini <= snd l 
+                          then menor_valor ini ls
+                          else menor_valor l ls
 
 menor :: Ord a1 => [(a2, a1)] -> (a2, a1)
 menor l = menor_valor (head l) l
 
+conta_removerElem _ [] = []
+conta_removerElem  n (l:ls) | n == l  = conta_removerElem  n ls
+                            | otherwise = l: conta_removerElem  n ls
 
-conta_removerElem :: (Eq a, Eq t, Num t) => t -> a -> [a] -> [a]
-conta_removerElem _ _ [] = []
-conta_removerElem c n (l:ls) | n == l && c == 0 = conta_removerElem (c + 1) n ls
-                             | otherwise = l: conta_removerElem c n ls
+removerElem n l = conta_removerElem  n l
 
-removerElem n l = conta_removerElem 0 n l
-
+ordenar :: [(Int, Palavra)] -> [(Int, Palavra)]
 ordenar [] = []
 ordenar l = menor l: ordenar (removerElem (menor l) l)
 
 --
 
+agrupar :: [(Int, Palavra)] -> [([Int], Palavra)]
 agrupar ls = [([fst n | n <- ls, (snd p) == (snd n)], snd p)| p <- ls]
 
+eliminarRep ::[([Int], Palavra)] -> [([Int], Palavra)]
 eliminarRep ls = repetidos ls
 
